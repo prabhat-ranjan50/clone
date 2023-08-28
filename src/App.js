df0d2497bcf1fca8c26ae8fff676cbe87200cb8e
Prabhat Ranjan <pprabhatranjan50@gmail.com>
2023-08-29 00:06:09 +0530

    Adding Homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     //BEM
     <div className="app">
       <Header/>
-      {/*Home*/}
+      <Home/>
     </div>
   );
 }
