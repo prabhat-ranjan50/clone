363800b903d5a362c9943919d7071022043588bf
Prabhat Ranjan <pprabhatranjan50@gmail.com>
2023-08-30 03:16:48 +0530

    Added React Router for Checkout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 
 function App() {
   return (
     //BEM
-    <div className="app">
-      <Header/>
-      <Home/>
-    </div>
+    <Router>
+        <div className="app">
+            <Switch>
+
+                <Route path='/checkout'>
+                    <Header/>
+                    <h1>I am the checkout</h1>
+                </Route>
+
+                <Route path='/'>
+                    <Header/>
+                    <Home/>
+                </Route>
+
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
